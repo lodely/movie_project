c363d40a5ee456ada939b9cdf4e96cf1e88dd7c5
lodely <lodely0306@gmail.com>
2018-07-31 15:31:33 +0800

    changeconfig

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,17 @@
 
 from flask import Flask, render_template
 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.debug = True
+
+# 配置连接的数据库
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+cymysql://root:root@localhost:3306/movie_project'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = '205YWHhaG0ASHGghaoghaQA0TGAHLHG'
+
+db = SQLAlchemy(app)
 
 from app.admin import admin as admin_blueprint
 from app.home import home as home_blueprint
