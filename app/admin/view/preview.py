22c245c337d8863d85039b2a4f8544f958cda153
lodely <lodely0306@gmail.com>
2018-08-02 10:15:52 +0800

    add_preview

--- app/admin/view/preview.py
+++ app/admin/view/preview.py
@@ -48,3 +48,40 @@ def preview_list(page=None):
         page=1
     previews = Preview.get_ten_previews(page=page)
     return render_template("admin/preview_list.html", previews=previews)
+
+# 预告删除
+@admin.route("/preview/del/<int:id>", methods=['GET'])
+@admin_login_req
+def preview_del(id=None):
+    preview = Preview.query.filter_by(id=id).first_or_404()
+    if preview:
+        with db.auto_commit():
+            db.session.delete(preview)
+        return redirect(url_for('admin.preview_list', page=1))
+    return render_template("admin/preview_list.html")
+
+# 编辑预告
+@admin.route("/preview/edit/<int:id>", methods=['GET', 'POST'])
+@admin_login_req
+def preview_edit(id=None):
+    form = PreviewForm()
+    if form.validate_on_submit():
+        data = form.data
+        preview = Preview.query.filter_by(id=id).first()
+        with db.auto_commit():
+            # 修改logo名
+            file_logo = secure_filename(form.logo.data.filename)
+            if not os.path.exists(app.config["UP_DIR"]):
+                    os.makedirs(app.config["UP_DIR"])
+                    os.chmod(app.config["UP_DIR"], "rw")
+
+            logo = change_filename(file_logo)
+            # 保存logo图片
+            form.logo.data.save(app.config["UP_DIR"]+logo)
+
+            preview.title=data['title'],
+            preview.logo=logo
+
+            db.session.add(preview)
+        return redirect(url_for("admin.preview_add"))
+    return render_template("admin/preview_add.html", form=form)
