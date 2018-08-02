ad3f87110317fe8e34c20b4ec5aa9060cb3e9f13
lodely <lodely0306@gmail.com>
2018-08-02 14:47:04 +0800

    add_commit

--- app/admin/view/comment.py
+++ app/admin/view/comment.py
@@ -7,10 +7,25 @@ from flask import render_template, redirect, url_for, flash, session, request
 
 from app.admin.base import admin_login_req
 from app.admin import admin
+from app.models import Comment, db
 
 
 # 评论列表
-@admin.route("/comment/list")
+@admin.route("/comment/list/<int:page>", methods=['GET'])
 @admin_login_req
-def comment_list():
+def comment_list(page=0):
+    if not page:
+        page = 1
+    comments = Comment.get_ten_comments(page=page)
+    return render_template("admin/comment_list.html", comments=comments)
+
+# 评论删除
+@admin.route("/movie/del/<int:id>", methods=['GET'])
+@admin_login_req
+def comment_del(id=None):
+    comment = Comment.query.filter_by(id=id).first_or_404()
+    if comment:
+        with db.auto_commit():
+            db.session.delete(comment)
+        return redirect(url_for("admin.comment_list", page=1))
     return render_template("admin/comment_list.html")
