60ed037a99c1bc07c156aff8c063051138b20f1a
lodely <lodely0306@gmail.com>
2018-08-02 13:01:25 +0800

    remove_oplog

--- app/admin/view/log.py
+++ app/admin/view/log.py
@@ -7,9 +7,20 @@ import os
 
 from flask import render_template, redirect, url_for, flash, session, request
 
-
 from app.admin.base import admin_login_req
 from app.admin import admin
+
+# 操作日志列表
+@admin.route("/oplog/list")
+@admin_login_req
+def oplog_list():
+    return render_template("admin/oplog_list.html")
+
+# 会员登录日志列表
+@admin.route("/userloginlog/list")
+@admin_login_req
+def userloginlog_list():
+    return render_template("admin/userloginlog_list.html")
 
 # 管理员登录日志列表
 @admin.route("/adminloginlog/list")
